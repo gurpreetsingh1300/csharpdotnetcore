08d3e90085ecd963569989e9de22dff1e7629bd6
gurpreetsingh1300 <gurpreetsinghsethi13@gmail.com>
2019-05-04 21:29:58 +0530

    sql copy saved

---
+++ SqlDB/Mine/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ SqlDB/Mine/OdeToFood.Data/SqlRestuarantData.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestuarantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestuarantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public Restaurant AddRestaurant(Restaurant addNewRestaurant)
+        {
+            db.Add(addNewRestaurant);
+            return addNewRestaurant;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurant DeleteRestaurant(int Id)
+        {
+            var restaurant = GetRestaurantById(Id);
+            if(restaurant != null)
+            {
+                db.Restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            var restaurants = from r in db.Restaurants
+                              orderby r.Name
+                              select r;
+            return restaurants;
+        }
+
+        public Restaurant GetRestaurantById(int Id)
+        {
+            return db.Restaurants.Find(Id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var restaurants = from r in db.Restaurants
+                              where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                              orderby r.Name
+                              select r;
+            return restaurants;
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant updatedRestaurant)
+        {
+            var entity = db.Restaurants.Attach(updatedRestaurant);
+            entity.State = EntityState.Modified;
+            return updatedRestaurant;
+        }
+    }
+}
