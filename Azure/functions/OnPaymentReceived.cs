11158e17e71142c5f152ef3cffab0e09c50e3d16
Gurpreet Sethi (Infosys Ltd) <v-guseth@microsoft.com>
2019-05-08 10:50:35 +0530

    added table

--- Azure/functions/OnPaymentReceived.cs
+++ Azure/functions/OnPaymentReceived.cs
@@ -16,6 +16,7 @@ namespace functions
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            [Table("orders")] IAsyncCollector<Order> orderTable,
             ILogger log)
         {
             log.LogInformation("Payment Received");
@@ -25,6 +26,11 @@ namespace functions
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var order = JsonConvert.DeserializeObject<Order>(requestBody);
             await orderQueue.AddAsync(order);
+
+            order.PartitionKey = "orders";
+            order.RowKey = order.OrderId;
+            await orderTable.AddAsync(order);
+
             log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
             //name = name ?? data?.name;
 
@@ -36,6 +42,8 @@ namespace functions
     }
     public class Order
     {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
         public string OrderId { get; set;}
         public string ProductId { get; set;}
         public string Email { get; set;}
