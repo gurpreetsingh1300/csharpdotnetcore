2b9d50ded215dd362d032687060a741cdc406b81
Gurpreet Singh Sethi (Infosys Ltd) <v-guseth@microsoft.com>
2019-05-08 12:11:05 +0530

    Completed without timer

--- Azure/functions/EmailLicenseFile.cs
+++ Azure/functions/EmailLicenseFile.cs
@@ -11,21 +11,25 @@ namespace functions
     public static class EmailLicenseFile
     {
         [FunctionName("EmailLicenseFile")]
-        public static void Run([BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")]string licenseFileContents, 
-            [SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message , string name, ILogger log)
+        public static void Run([BlobTrigger("licenses/{orderId}.lic",
+            Connection = "AzureWebJobsStorage")]string licenseFileContents,
+            [SendGrid(ApiKey = "SendGridApiKey")] ICollector<SendGridMessage> sender,
+            [Table("orders", "orders", "{orderId}")] Order order,
+            string orderId,
+            ILogger log)
         {
-            var email = Regex.Match(licenseFileContents, @"^Email\:\ (.+)$",RegexOptions.Multiline).Groups[1].Value;
-            log.LogInformation($"Got order from {email}");
-
-            message = new SendGridMessage();
+            var email = order.Email; //Regex.Match(licenseFileContents, @"^Email\:\ (.+)$",RegexOptions.Multiline).Groups[1].Value;
+            log.LogInformation($"Got order from {email}\n Order Id:{orderId}");
+            var message = new SendGridMessage();
             message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
             message.AddTo(email);
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContents);
             var base64 = Convert.ToBase64String(plainTextBytes);
-            message.AddAttachment(name, base64, "text/plain");
+            message.AddAttachment($"{orderId}.lic", base64, "text/plain");
             message.Subject = "Your license file";
             message.HtmlContent = "Thank you for your order";
-
+            if (!email.EndsWith("@test.com"))
+                sender.Add(message);
         }
     }
 }
--- Azure/functions/GenerateLicenseFile.cs
+++ Azure/functions/GenerateLicenseFile.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,8 +10,14 @@ namespace functions
     public static class GenerateLicenseFile
     {
         [FunctionName("GenerateLicenseFile")]
-        public static void Run([QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order, [Blob("licenses/{rand-guid}.lic")]TextWriter outputBlob, ILogger log)
+        public static async Task Run([QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order, IBinder binder, ILogger log) //alt way for binding - [Blob("licenses/{rand-guid}.lic")]TextWriter outputBlob,
         {
+            var outputBlob = await binder.BindAsync<TextWriter>(
+                new BlobAttribute($"licenses/{order.OrderId}.lic")
+                {
+                    Connection = "AzureWebJobsStorage"
+                });
+
             outputBlob.WriteLine($"OrderId: {order.OrderId}");
             outputBlob.WriteLine($"Email: {order.Email}");
             outputBlob.WriteLine($"ProductId: {order.ProductId}");
