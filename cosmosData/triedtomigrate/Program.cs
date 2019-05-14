1339cba76751cf4e1d439f103f09d673fceb0769
Gurpreet Singh Sethi (Infosys Ltd) <v-guseth@microsoft.com>
2019-05-14 11:49:41 +0530

    restructured

---
+++ cosmosData/triedtomigrate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+
+namespace JsonAdd
+{
+    class Program
+    {
+        private Uri restaurantsLink;
+        private DocumentClient client;
+        static void Main(string[] args)
+        {
+            List<dynamic> objs = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText("rest.json"));
+            JsonAddClass jsonObj = new JsonAddClass();
+            jsonObj.Add(objs);
+
+
+        }
+        
+    }
+}
